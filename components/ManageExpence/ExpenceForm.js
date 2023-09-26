d6f50d59dc810f30fae7c1554b30dff715bafb5f
tahseen_IO <tahseen@mogiio.com>
2023-09-27 01:31:26 +0530

    changes in input

--- components/ManageExpence/ExpenceForm.js
+++ components/ManageExpence/ExpenceForm.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import Input from './Input';
 
@@ -7,21 +7,23 @@ export default function ExpenceForm() {
 
   return (
     <View>
-      <Input
-        label={'Amount'}
-        textInputConfig={{
-          keyboardType: 'decimal-pad',
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label={'Date'}
-        textInputConfig={{
-          placeholder: 'YYYY-MM-DD',
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+      <View style={styles.inputRow}>
+        <Input
+          label={'Amount'}
+          textInputConfig={{
+            keyboardType: 'decimal-pad',
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          label={'Date'}
+          textInputConfig={{
+            placeholder: 'YYYY-MM-DD',
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
       <Input
         label={'Description'}
         textInputConfig={{
@@ -31,3 +33,10 @@ export default function ExpenceForm() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
