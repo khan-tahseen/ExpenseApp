7af8a8e63a9aa260519241400939516fcd35b908
tahseen_IO <tahseen@mogiio.com>
2023-09-26 12:55:55 +0530

    Confirm and Cancle button added

--- UI/Button.js
+++ UI/Button.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
   },
   pressed: {
     opacity: 0.75,
-    backgroundColor: GlobalStyles.colors.primary100,
+    backgroundColor: GlobalStyles.colors.primary200,
     borderRadius: 8,
   },
 });
--- screens/ManageExpence.js
+++ screens/ManageExpence.js
@@ -2,6 +2,7 @@ import { View, StyleSheet } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import IconButton from '../UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
+import Button from '../UI/Button';
 
 export default function ManageExpence({ route, navigation }) {
   const editedExpenceId = route.params?.expenceId;
@@ -13,10 +14,22 @@ export default function ManageExpence({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
+  function cancleHandler() {}
+
+  function confirmHandler() {}
+
   function deleteExpenceHandler() {}
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancleHandler}>
+          Cancle
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -36,6 +49,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     borderTopWidth: 2,
